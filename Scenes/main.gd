d48b503e6b4003bd1fe73d06e4e56e56bc747095
Jon Harder <jonharder6@gmail.com>
2019-06-05 16:40:28 -0500

    make hud scene

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _ready():
-	$Cursor.connect("cursor_moved", self, "_on_cursor_moved")
+	$HUD.connect("cursor_moved", self, "_on_cursor_moved")
 	
 func _on_cursor_moved(location : Vector2):
 	print(location)
